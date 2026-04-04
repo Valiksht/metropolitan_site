76a6e7965b9539d5ccce28c23899fb012565b911
Valentin Rubanov <rubanov.valentin@ya.ru>
2026-04-04 12:48:38 +0300

    work site with ssl

--- cstovo/cstovo/settings.py
+++ cstovo/cstovo/settings.py
@@ -7,12 +7,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
-# DEBUG = os.getenv("DEBUG_STATUS", "False") == "True"
-DEBUG = True
+DEBUG = os.getenv("DEBUG_STATUS", "True") == "True"
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     os.getenv('ALLOWED_HOSTS_ID'),
     os.getenv('ALLOWED_HOSTS_URL'),
+    os.getenv('ALLOWED_HOSTS_URL_WWW'),
     'localhost',
     '127.0.0.1'
 ]
